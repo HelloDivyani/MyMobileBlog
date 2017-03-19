929263ac186d0ec13fb1a1e5d9700b17e2809b0a
Divyani Mittal <hellodivyani@gmail.com>
2017-03-19 11:11:39 +0530

    Added api

---
+++ app/src/main/java/com/example/android/myblogapp/AuthenticationRequest.java
@@ -0,0 +1,7 @@
+package com.example.android.myblogapp;
+
+/**
+ * Created by Divyani on 19-03-2017.
+ */
+public class AuthenticationRequest {
+}
---
+++ app/src/main/java/com/example/android/myblogapp/NetworkURL.java
@@ -0,0 +1,13 @@
+package com.example.android.myblogapp;
+
+/**
+ * Created by Divyani on 19-03-2017.
+ */
+public class NetworkURL
+{
+    // Base url as static so that it can be called without making object or with class name
+
+    public static final String BASEURL ="http://hellodivyani.imad.hasura-app.io";
+    private  static final String Login="login";
+    private static final String Registeration="create-user";
+}
